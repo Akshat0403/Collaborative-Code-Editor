b2648490389dd68d7035da43ea4e37c0d222c7fc
Akshat Saxena <bluakshat0403@gmail.com>
2025-11-27 00:32:08 +0530

    Project Completed

--- src/app/pricing/_components/UpgradeButton.tsx
+++ src/app/pricing/_components/UpgradeButton.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 export default function UpgradeButton() {
   const CHEKOUT_URL =
-    "https://ytprogrammingstore.lemonsqueezy.com/buy/d459dddb-a233-4060-9e72-90a1a7740552";
+    "https://devcollab.lemonsqueezy.com/buy/87cfacff-5f6f-4521-9159-473678f119c8";
 
   return (
     <Link
